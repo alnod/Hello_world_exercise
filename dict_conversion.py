41a168a02b96ed328e9e6d6909318e4d767aad3e
alnod <sds3104022025@students.uonbi.ac.ke>
2025-10-13 18:45:19 +0300

    Dictionary conversion

---
+++ dict_conversion.py
@@ -0,0 +1,24 @@
+"""
+-create a function that sorts items in a list and stores them in a dictionary. 
+- key is the index, values remain the same.
+[2, 3, 5, 8, 1, 4]
+- sort
+- store in dict {key: value}
+return dict
+- push to github in a new branch - dict_values
+- create a pull request
+"""
+
+def type_conversion(arr):
+    # print(arr)
+    arr.sort()
+    # print(arr)
+    new_dict = {}
+    for ind, val in enumerate(arr):
+        new_dict[ind] = val
+    print(new_dict)
+
+
+
+num = [2, 3, 5, 8, 1, 4]
+type_conversion(num)
